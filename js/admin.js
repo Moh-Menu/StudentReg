18a4056b6d4cd045f7fdb60e3470df0e204a9f0d
Moh-Menu <beldaje02@gmail.com>
2025-11-27 15:07:36 +0300

    ss

--- js/admin.js
+++ js/admin.js
@@ -1,12 +1,1 @@
-function saveLoginStyle() {
-    let selected = document.querySelector("input[name='loginStyle']:checked");
 
-    if (!selected) {
-        document.getElementById("statusText").innerText = "Please select a style.";
-        return;
-    }
-
-    localStorage.setItem("loginStyle", selected.value);
-
-    document.getElementById("statusText").innerText = "Login style saved!";
-}
